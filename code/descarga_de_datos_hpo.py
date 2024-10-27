a1c68c8b26bc1ca255fd5f7660dca02bd514894a
Alexsilvaa9 <alexsilvaebg9@gmail.com>
2024-10-27 20:02:43 +0100

    script para carga de datos de hpo creado

---
+++ code/descarga_de_datos_hpo.py
@@ -0,0 +1,18 @@
+import requests
+
+# URL base del HPO
+HPO="HP:0031993"
+HPO_GENES=f"https://ontology.jax.org/api/network/annotation/{HPO}/download/gene"
+HPO_DISEASES=f"https://ontology.jax.org/api/network/annotation/{HPO}/download/disease"
+def download_data(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+        print(f"Data successfully downloaded to {filename}")
+    else:
+        print(f"Failed to download data. Status code: {response.status_code}")
+
+# Descargar genes y enfermedades
+download_data(HPO_GENES, "genes.tsv")
+download_data(HPO_DISEASES, "diseases.tsv")
