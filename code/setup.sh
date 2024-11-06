a3e2389bdf9c46c4b27ec16d143b42c12f05fd81
MartaCuevas <marcuerodri@gmail.com>
2024-11-06 20:11:05 +0100

    creado set up

--- code/setup.sh
+++ code/setup.sh
@@ -1,2 +1,18 @@
+#!/bin/bash
+
+# Definir las carpetas donde estarán las dependencias de python y de R
+export R_LIB: ./R_packages
+mkdir_p$R_LIB
+R.script_e "install.packages(Pack)"
+
+export PYTHON_LIB=./py_packages
 pip install -r requirements.txt
 
+# Limpieza de cache
+python -m pip cache purge
+
+# Instalar dependencias de R del archivo Rreqs.txt en la carpeta R
+Rscript -e 'install.packages(readLines("Rreqs.txt"), lib="./R_packages")'
+
+# Imprimir mensaje de finalización
+echo "Instalación de dependencias completada con éxito.
