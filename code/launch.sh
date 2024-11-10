cd528a4775d3ed308bb2104bc2bde7d7e23a5247
Alexsilvaa9 <alejandrosilvarodriguez9@gmail.com>
2024-11-10 01:47:14 +0100

    arreglado y mejorado setup.sh y launch.sh

--- code/launch.sh
+++ code/launch.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-export R_LIB: ./R_packages
+# Definir las carpetas donde estarán las dependencias de Python y de R
+export R_LIB="./R_packages"
+export PYTHON_LIB="./py_packages"
 
-export PYTHON_LIB=./py_packages
-
+# Crear directorios necesarios
 mkdir -p data
 
-# Descarga de datos de genes de HPO
-wget https://ontology.jax.org/api/network/annotation/HP:0031993/download/gene -o /data/genes.tsv
-# Pasamos de genes a string id
-# python genes2string.py /data/genes.tsv /data/genes_string.tsv
+# Descargar los datos de genes de HPO
+wget https://ontology.jax.org/api/network/annotation/HP:0031993/download/gene -O data/genes.tsv
 
-# Desacarga de la red de String
-wget https://stringdb-downloads.org/download/protein.links.v12.0/9606.protein.links.v12.0.txt.gz -o /data/network.txt
+# Convertir genes a string id (descomentar cuando el script esté listo)
+# python genes2string.py data/genes.tsv data/genes_string.tsv
 
-# Propagación de genes
-# python dyamond.py /data/string.tsv /data/network.txt /data/red_propagada.txt
+# Descargar la red de String
+wget https://stringdb-downloads.org/download/protein.links.v12.0/9606.protein.links.v12.0.txt.gz -O data/network.txt.gz
 
-# Análisis de red
-# python analisis.py /data/red_propagada.txt /data/clusters.txt
+# Descomprimir el archivo descargado
+gunzip data/network.txt.gz
 
-# Análisis de enriquecimiento funcional
-# python analisis_enriquecimiento.py /data/clusters.txt 
+# Propagación de genes (descomentar cuando el script esté listo)
+# python dyamond.py data/genes_string.tsv data/network.txt data/red_propagada.txt
+
+# Análisis de la red (descomentar cuando el script esté listo)
+# python analisis.py data/red_propagada.txt data/clusters.txt
+
+# Análisis de enriquecimiento funcional (descomentar cuando el script esté listo)
+# python analisis_enriquecimiento.py data/clusters.txt
--- code/setup.sh
+++ code/setup.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Definir las carpetas donde estarán las dependencias de python y de R
-export R_LIB: ./R_packages
-mkdir_p$R_LIB
-R.script_e "install.packages(Pack)"
+# En caso de que ocurra un error, detener la ejecución del script
+set -e
 
-export PYTHON_LIB=./py_packages
-pip install -r requirements.txt
+# Definir las carpetas donde estarán las dependencias de Python y de R
+export R_LIB="./R_packages"
+mkdir -p $R_LIB
 
-# Limpieza de cache
+export PYTHON_LIB="./py_packages"
+mkdir -p $PYTHON_LIB
+
+# Instalar dependencias de Python en el directorio personalizado
+pip install --prefix=$PYTHON_LIB -r requirements.txt
+
+# Limpieza de caché
 python -m pip cache purge
 
 # Instalar dependencias de R del archivo Rreqs.txt en la carpeta R
 Rscript -e 'install.packages(readLines("Rreqs.txt"), lib="./R_packages")'
 
 # Imprimir mensaje de finalización
-echo "Instalación de dependencias completada con éxito.
-
+echo "Instalación de dependencias completada con éxito."
