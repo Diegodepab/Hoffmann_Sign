e486a0b0a807877dc3ee125b8591360b0afe5019
Alexsilvaa9 <alejandrosilvarodriguez9@gmail.com>
2024-11-28 22:49:28 +0100

    ya se guardan resultados en /results no en code/results

--- code/analisis_enriquecimiento.py
+++ code/analisis_enriquecimiento.py
@@ -8,8 +8,7 @@ import argparse
 # Asegúrate de que los directorios necesarios existan
 if not os.path.exists('data/enrichment_analysis'):
     os.makedirs('data/enrichment_analysis')
-if not os.path.exists('../results/enrichment_analysis'):
-    os.makedirs('../results/enrichment_analysis')
+
 
 def enrichment_analysis_enrichr_keeg_func(genes_by_cluster, combined_score_threshold=40):
     # URLs de la API de Enrichr
